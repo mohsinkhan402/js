b68f032b2122cf4dc888f0667c1de8c7a6863032
mohsinkhan402 <109978712+mohsinkhan402@users.noreply.github.com>
2024-07-02 13:11:36 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";    // treat all js code as newer virsion 
+//alert(3+3)    //we are using nodejs not browser
+
+console.log(3
+    +
+3); // code readability should be high
+
+
+// DATATYPES
+
+// number => 2 to power53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stantalone value
+// undefine
+// symbol =>unique
+
+
+// object
+console.log(typeof "mohsin") // gives string
+
+console.log(typeof null)  // gives object
+console.log(typeof undefined) // gives undefined
+
+
