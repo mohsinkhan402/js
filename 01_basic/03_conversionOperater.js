fef8d378ebe26c1a3613d6f6442c57d9e3267b5b
mohsinkhan402 <109978712+mohsinkhan402@users.noreply.github.com>
2024-07-07 06:47:00 +0000

    conversionOperater finish

---
+++ 01_basic/03_conversionOperater.js
@@ -0,0 +1,66 @@
+let score = "30abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// input => output
+
+// "33" => 33
+// "33asc" => NaN (NaN ka type number hota he)
+//  true => 1; false =>0;
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn); 
+// input =>output
+// 1 => true;
+// 0 =>false;
+// "mohsin" => true;
+
+// ***************Operations*****************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2); // 2 ki power 2
+// console.log(2**#); // 2 ki power 3
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 ="hii";
+let str2 = " mohsin";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);        // 12
+// console.log("1" + 2 + 3);    // 123
+// console.log(1 + 2 + "2");    // 32
+
+// console.log((1+2) * 5 % 2);  // 1 modules
+
+
+// console.log(+true);
+// console.log(+"");
+
+ let num1,num2,num3;
+ num1 = num2 = num3 = 2 + 2;  // not recomended
+
+
+ let gameCounter = 100;
+ gameCounter++;
+ console.log(gameCounter);
+
+
+
+
