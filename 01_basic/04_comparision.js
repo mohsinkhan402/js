749a302594e9ab23334fad0b00767ab2b56b3a27
mohsinkhan402 <109978712+mohsinkhan402@users.noreply.github.com>
2024-07-10 06:14:44 +0000

    complede comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(1 < 2);
+// console.log(1 > 2);
+// console.log(1 == 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 != 2);
+
+// console.log(1 < "2");  true
+// console.log(1 < "02"); true
+
+// console.log(null >= 0);   true
+// console.log(null == 0);   false
+// console.log(null < 0);    false  all comparisions(except ==) sign convert null into 0 
+// console.log(null != 0);   true
+
+// console.log(undefined == 0);  false
+// console.log(undefined < 0);   false
+// console.log(undefined > 0);   false
+
+// try to avoid above code   use strict check "===" //
+
+// console.log("2" === 2);   false 
+
+
+
+
