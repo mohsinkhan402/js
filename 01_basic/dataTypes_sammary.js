8e8f01e072860ee0aa250a1a83fb42b78b389354
mohsinkhan402 <109978712+mohsinkhan402@users.noreply.github.com>
2024-07-10 08:31:47 +0000

    complete summary

---
+++ 01_basic/dataTypes_sammary.js
@@ -0,0 +1,27 @@
+
+//  Primitive
+
+//   7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+
+// console.log(id == anotherId);  false
+// console.log(id === anotherId); false
+
+//  Reference (Non Peimitive)
+
+// Object, Array, function
+
+// const heros = ["shahrukh khan",'salman khan','akshey kumar'];    // array
+
+// let myInfo = {
+//     name: "mohsin",
+//     age : "26"
+
+// }
+// const myFunction(){
+//     console.log("Mohd Mohsin");
+// }
+
+// console.log(typeof(myFunction))  output:function
