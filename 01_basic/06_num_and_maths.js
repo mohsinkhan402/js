8c8f3401ce99e4dd53dfaffd3c7ee8efd086184d
mohsinkhan402 <109978712+mohsinkhan402@users.noreply.github.com>
2024-07-20 09:58:00 +0000

    complete math and numbers

---
+++ 01_basic/06_num_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400
+
+const balance = new Number(100);
+
+// console.log(score); //400
+// console.log(balance);  //[Number: 100]
+// console.log(balance.toString().length); //3
+
+// console.log(balance.toFixed(1));   //100.0
+
+const number1 = 23.566;
+// console.log(number1.toPrecision(4));  //23.57
+
+const number2 = 10000000;
+
+// console.log(number2.toLocaleString());   //10,000,000
+// console.log(number2.toLocaleString('en-IN'));   //1,00,00,000
+
+// ****************************maths**********************
+
+
+// console.log(Math);  // Object [Math] {}
+// console.log(Math.abs(-5));  //  5
+
+// console.log(Math.round(5.9));   // 6
+
+// console.log(Math.ceil(4.3));   // 5
+
+// console.log(Math.floor(4.9));   // 4
+
+// console.log(Math.random());        //0.35519765321734886
+// console.log(Math.random() * 10)   // 3.68309277003001
+
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random()* (max - min + 1)) + min );
